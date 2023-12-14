3e069eac579d0383bf2896b5e6ecfb5c53ee90e5
khs <ewoking777@gmail.com>
2023-12-14 23:15:14 +0900

    섹션 3. 연산자 : 산술 연산자

---
+++ src/operator/Operator1.java
@@ -0,0 +1,31 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        //변수 초기화
+        int a = 5;
+        int b = 2;
+
+        //덧셈
+        int sum = a + b;
+        System.out.println("a + b = " + sum); //출력 a + b = 7
+
+        //뺄셈
+        int diff = a - b;
+        System.out.println("a - b = " + diff);
+
+        //곱셈
+        int nulti = a * b;
+        System.out.println("a * b = " + nulti);
+
+        //나눗셈
+        int div = a / b;
+        System.out.println("a / b = " + div);
+
+        //나머지
+        int mod = a % b;
+        System.out.println("a % b = " + mod);
+
+        int z = 10 / 0;
+    }
+}
